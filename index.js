432135f1dea1123d1f15aab6f83bf5604720c828
Alyson Consuli <consulialyson@gmail.com>
2022-04-22 12:52:28 -0300

    feat: tratamento completo dos posts e do get

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-import express from "express";
+import express from "express"
 import cors from 'cors'
 
 const app = express()
+app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Teste')
+const users = []
+const tweets = []
+
+app.post('/sign-up', (req, res) => {
+    const user = req.body
+    users.push(user)
+    res.send('OK')
+})
+
+app.post('/tweets', (req, res) => {
+    const user = users[users.length - 1]
+    const {tweet} = req.body
+    tweets.push({...user, tweet})
+    res.send('OK')
+})
+
+app.get('/tweets', (req, res) => {
+    (tweets.length > 10 && tweets.shift())
+    const recentTweets = tweets.slice(0).reverse()
+    res.send(recentTweets)
 })
 
 app.listen(5000, () => console.log("Server is running on: http://localhost:5000"))
