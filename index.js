ab301e66fc2c7890ff9d6892eddfadb5913c6ace
Alyson Consuli <consulialyson@gmail.com>
2022-04-25 13:11:37 -0300

    feat: Todos os requisitos bonus

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const tweets = []
 app.post('/sign-up', (req, res) => {
     const user = req.body
     if(user.username === '' || user.avatar === ''){
-        //res.sendStatus(400)
         return res.status(400).send('Todos os campos são obrigatórios!')
     }
     users.push(user)
@@ -19,19 +18,42 @@ app.post('/sign-up', (req, res) => {
 })
 
 app.post('/tweets', (req, res) => {
-    const user = users[users.length - 1]
+    let username = req.header('User')
+    if(!username){
+        username = users[users.length - 1].username
+    }
+    const {avatar} = users[users.length - 1]
     const {tweet} = req.body
-    if(user.username === '' || tweet === ''){
+    if(username === '' || tweet === ''){
         return res.status(400).send('Todos os campos são obrigatórios!')
     }
-    tweets.push({...user, tweet})
+    tweets.push({
+        username,
+        avatar,
+        tweet
+    })
     res.status(201).send('Created')
 })
 
 app.get('/tweets', (req, res) => {
-    (tweets.length > 10 && tweets.shift())
+    const {page} = req.query
     const recentTweets = tweets.slice(0).reverse()
-    res.send(recentTweets)
+    if(!page){
+        const lastTenTweets = recentTweets.filter((tweet, i) => i < 10)
+        return res.send(lastTenTweets)
+    }
+    if(page < 1){
+        return res.status(400).send('Informe uma página válida!')
+    }
+    const tweetsPage = recentTweets.filter((tweet, i) => i < page*10 && i >= page*10 - 10)
+    res.send(tweetsPage)
+})
+
+app.get('/tweets/:username', (req, res) => {
+    const {username} = req.params
+    const recentTweets = tweets.slice(0).reverse()
+    const userTweets = recentTweets.filter(tweet => tweet.username === username)
+    res.send(userTweets)
 })
 
 app.listen(5000, () => console.log("Server is running on: http://localhost:5000"))
