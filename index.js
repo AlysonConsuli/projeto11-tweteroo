fed5fe56cf472b57ec40c15f385d5da804edc800
Alyson Consuli <consulialyson@gmail.com>
2022-04-25 16:47:26 -0300

    fix: resposta do server caso nao encontre o usuario no post & statusCode mais especificos

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express from 'express'
 import cors from 'cors'
 
 const app = express()
@@ -10,22 +10,26 @@ const tweets = []
 
 app.post('/sign-up', (req, res) => {
     const user = req.body
-    if(user.username === '' || user.avatar === ''){
-        return res.status(400).send('Todos os campos são obrigatórios!')
+    if (user.username === '' || user.avatar === '') {
+        return res.status(422).send('Fill all fields')
     }
     users.push(user)
     res.status(201).send('Created')
 })
 
 app.post('/tweets', (req, res) => {
-    let username = req.header('User')
-    if(!username){
-        username = users[users.length - 1].username
+    const user = users[users.length - 1]
+    if (!user) {
+        return res.status(404).send('User not found');
     }
-    const {avatar} = users[users.length - 1]
-    const {tweet} = req.body
-    if(username === '' || tweet === ''){
-        return res.status(400).send('Todos os campos são obrigatórios!')
+    let username = req.header('User')
+    if (!username) {
+        username = user.username
+    }
+    const { avatar } = user
+    const { tweet } = req.body
+    if (!username || !tweet) {
+        return res.status(422).send('Fill all fields')
     }
     tweets.push({
         username,
@@ -36,24 +40,24 @@ app.post('/tweets', (req, res) => {
 })
 
 app.get('/tweets', (req, res) => {
-    const {page} = req.query
+    const { page } = req.query
     const recentTweets = tweets.slice(0).reverse()
-    if(!page){
+    if (!page) {
         const lastTenTweets = recentTweets.filter((tweet, i) => i < 10)
         return res.send(lastTenTweets)
     }
-    if(page < 1){
-        return res.status(400).send('Informe uma página válida!')
+    if (page < 1) {
+        return res.status(422).send('Inform a valid page!')
     }
-    const tweetsPage = recentTweets.filter((tweet, i) => i < page*10 && i >= page*10 - 10)
+    const tweetsPage = recentTweets.filter((tweet, i) => i < page * 10 && i >= page * 10 - 10)
     res.send(tweetsPage)
 })
 
 app.get('/tweets/:username', (req, res) => {
-    const {username} = req.params
+    const { username } = req.params
     const recentTweets = tweets.slice(0).reverse()
     const userTweets = recentTweets.filter(tweet => tweet.username === username)
     res.send(userTweets)
 })
 
-app.listen(5000, () => console.log("Server is running on: http://localhost:5000"))
+app.listen(5000, () => console.log('Server is running on: http://localhost:5000'))
